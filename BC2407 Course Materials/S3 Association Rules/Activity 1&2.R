9ba1e1aa1d8fd2bfcb4e090c6ce329e33985895f
ngjunlong9651 <42053960+ngjunlong9651@users.noreply.github.com>
2023-02-01 11:27:42 +0800

    Added mlxtend other metrics to measure apriori rules

--- BC2407 Course Materials/S3 Association Rules/Activity 1&2.R
+++ BC2407 Course Materials/S3 Association Rules/Activity 1&2.R
@@ -8,36 +8,21 @@ setwd("/Users/junlongng/Desktop/NTU/Year_2/Semester 2/BC2407 Analytics II/BC2407
 
 df = read.csv("/Users/junlongng/Desktop/NTU/Year_2/Semester 2/BC2407 Analytics II/BC2407 Course Materials/S3 Association Rules/milk.csv")
 
-## We shall start with the longdata format
+## We shall start with splitting the data into two format
+widedata = df
 longdata = df
-longdata
 longdata = melt(data = longdata, id.vars = "ID")
 longdata = subset(longdata, value != 0)
 ## Removing the ID & Index column
 longdata = longdata[,1:2]
 
-
-## We will now convert long to wide data type 
-widedata = df
-widedata
-## Need to remove the index column: ID each row has been considered as a transaction. Hence index column is redundant as an identifier.
-widedata = widedata[,-1]
-
-
-
-
-## -------------------- Activity 2 -------------- ##
-
-#Converting from dataframe to transactions datatype using arules package
-
-widedata[] = lapply(widedata,factor)
-## All columns to factor datatype instead of integer & save it as a dataframe
-
+## Changing columns to factorial data type instead of integer values. Essentially "encoding"
 summary(widedata)
-
-## Checking that all are factorial data type
+widedata[] = lapply(widedata,factor)
+summary(widedata)
+widedata = widedata[,-1] ## Removing ID field, not really needed for analysis
 str(widedata)
-
+## 5 variables -> all factor datatype with 2 levels.
 
 
 ## Converting the datatype to a transaction data format
@@ -57,6 +42,7 @@ inspect(trans)
 ## Trying it out with a wide data
 ruleswide = apriori(data = trans, parameter = list(minlen = 2, supp = 0.4, conf = 0.3, target = "rules"))
 summary(ruleswide)
+
 ## This returns us a set of 65 rules.
 ## From these 65 rules, we shall inspect the top 10 rules by support, confidence, lift
 # inspect(head(ruleswide, n = 10, by ="support"))
@@ -68,15 +54,53 @@ rules.wide = inspect(ruleswide)
 ## Trying it out with a long data
 longdata
 
-## Converting Longdata to Widedata by formatting values to be logical
-longdata2 = data.frame(lapply(widedata, as.logical))
-longdata2 = longdata[,-1]
+## Exporting results of a longdata into a csv and calling the csv back into R studio.
+write.csv(longdata, "longdatatotransactions.csv", row.names = FALSE)
 
-trans2 = as(longdata2, "transactions")
-trans2
 
-ruleslong = apriori(data = trans2, parameter = list(minlen = 2, supp = 0.4, conf = 0.3, target = "rules"))
+long_trans = read.transactions("longdatatotransactions.csv",format = "single", sep=",", header = TRUE, cols =c("ID","variable"))
+
+## Take note that column has to match entries in the header of the file
+
+## Convert long datatype to transactions format
+long_trans = as(longdata, "transactions")
+
+summary((long_trans))
+inspect(long_trans)
+
+
+ruleslong = apriori(data = long_trans, parameter = list(minlen = 2, supp = 0.4, conf = 0.3, target = "rules"))
 
 summary(ruleslong)
 inspect(ruleslong)
 ## even though parameters values are the same, they give different results. Long data type produces lesser rules as compared to a wide data type. 
+
+
+
+## ------------------ Question 2 -----------------## 
+## Default parameters might produce an empty set of rules as we can see in the long data type, to rectify this, we can adjust the min support parameter. 
+## If you want to produce more rules using apriori algorithm, need to reduce min support to get more rules.
+ruleslong = apriori(data = long_trans, parameter = list(minlen = 2, supp = 0.1, conf = 0.3, target = "rules"))
+
+
+
+## ----------------- Question 3 -----------------##
+## a) 
+## What's the cause of the difference:
+
+
+
+## b)
+## Whan should you use long or wide data format:
+
+## Long Data:
+## Each row is a one-time point per subject, meaning that each row is not unique and each subject could contribute to multiple rows worth of data. Hence, long datasets are better for statistical analysis where they might be counting how much of each item is sold rather than the association between two items.
+
+## Wide Data:
+## In general, when analyzing data we should use a wide data set. This ensures that each entry in the data is unique and the subject's repeated responses will be in a single row and each response generates a separate column. Furthermore, wide data format is supported by ggplot2
+
+
+
+
+
+
